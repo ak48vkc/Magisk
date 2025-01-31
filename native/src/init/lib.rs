3922f486c92740cd3780c9b381367491d3d2deab
LoveSy <shana@zju.edu.cn>
2025-01-31 12:35:42 +0800

    Move MagiskInit::second_stage to rust

--- native/src/init/lib.rs
+++ native/src/init/lib.rs
@@ -12,6 +12,7 @@ mod mount;
 mod rootdir;
 mod getinfo;
 mod init;
+mod twostage;
 
 #[cxx::bridge]
 pub mod ffi {
@@ -85,7 +86,6 @@ pub mod ffi {
         // Two stage init
         fn redirect_second_stage(self: &MagiskInit);
         fn first_stage(self: &MagiskInit);
-        fn second_stage(self: &mut MagiskInit);
 
         // SELinux
         unsafe fn patch_sepolicy(self: &MagiskInit, in_: *const c_char, out: *const c_char);
