8310357133a408b3eed79b27a27cda6c7e6b0cb5
LoveSy <shana@zju.edu.cn>
2025-01-31 13:25:53 +0800

    Move MagiskInit::first_stage to rust

--- native/src/init/lib.rs
+++ native/src/init/lib.rs
@@ -85,7 +85,6 @@ pub mod ffi {
 
         // Two stage init
         fn redirect_second_stage(self: &MagiskInit);
-        fn first_stage(self: &MagiskInit);
 
         // SELinux
         unsafe fn patch_sepolicy(self: &MagiskInit, in_: *const c_char, out: *const c_char);
